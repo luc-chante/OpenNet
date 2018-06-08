4506ddaa7b5a1ec406eb23aed7ae87c902b7b534
Luc Chante <luc.chante@gmail.com>
2018-06-08 14:25:50 +0200

    Add ILoggingBuilder extension with configure action

--- src/Logging.File/FileLoggerExtension.cs
+++ src/Logging.File/FileLoggerExtension.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Logging.Configuration;
 using Microsoft.Extensions.Options;
 using QuadriPlus.Extensions.Logging.File;
+using System;
 
 namespace QuadriPlus.Extensions.Logging
 {
@@ -15,11 +16,29 @@ namespace QuadriPlus.Extensions.Logging
         /// <param name="builder">The <see cref="ILoggingBuilder"/> to use.</param>
         public static ILoggingBuilder AddFile(this ILoggingBuilder builder)
         {
-            builder.Services.AddSingleton<ILoggerProvider, FileLoggerProvider>();
+            builder.AddConfiguration();
 
             builder.Services.TryAddEnumerable(ServiceDescriptor.Singleton<ILoggerProvider, FileLoggerProvider>());
             builder.Services.TryAddEnumerable(ServiceDescriptor.Singleton<IConfigureOptions<FileLoggerOptions>, FileLoggerOptionsSetup>());
             builder.Services.TryAddEnumerable(ServiceDescriptor.Singleton<IOptionsChangeTokenSource<FileLoggerOptions>, LoggerProviderOptionsChangeTokenSource<FileLoggerOptions, FileLoggerProvider>>());
+            return builder;
+        }
+
+        /// <summary>
+        /// Adds a file logger named 'File' to the factory.
+        /// </summary>
+        /// <param name="builder">The <see cref="ILoggingBuilder"/> to use.</param>
+        /// <param name="configure"></param>
+        public static ILoggingBuilder AddConsole(this ILoggingBuilder builder, Action<FileLoggerOptions> configure)
+        {
+            if (configure == null)
+            {
+                throw new ArgumentNullException(nameof(configure));
+            }
+
+            builder.AddFile();
+            builder.Services.Configure(configure);
+
             return builder;
         }
     }
